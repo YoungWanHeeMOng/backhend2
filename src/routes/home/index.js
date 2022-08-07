2a73b806c6faeff6388ba5da8b228ceadf5b5342
youngwan kim <46891693+YoungWanHeeMOng@users.noreply.github.com>
2022-08-08 06:51:00 +0900

    login UI Make

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,6 +8,7 @@ const ctrl = require("./home.ctrl"); // MVC - 중간 제어
 router.get('/', ctrl.output.home);
 
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
 router.post("/login", ctrl.process.login);
 
 
