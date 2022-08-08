c770ad477260edecec847f34e2be91f83e69bbf9
youngwan kim <46891693+YoungWanHeeMOng@users.noreply.github.com>
2022-08-09 07:42:22 +0900

    AWS RDS 와 연동 & dotenv로 환경변수 등록

--- src/config/db.js
+++ src/config/db.js
@@ -2,10 +2,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "kywhdgo1",
-    database: "login_lecture",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASES,
 });
 
 db.connect();
